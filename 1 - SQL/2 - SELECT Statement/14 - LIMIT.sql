5c9098b0cfac44d8c6c60a126fec38c327eb29de
Kavi-Sarna <135118806+Kavi-Sarna@users.noreply.github.com>
2024-03-10 17:57:32 +0530

    LIMIT

---
+++ 1 - SQL/2 - SELECT Statement/14 - LIMIT.sql
@@ -0,0 +1,28 @@
+USE employees;
+
+SELECT 
+    *
+FROM
+    salaries;
+
+
+SELECT 
+    *
+FROM
+    salaries
+ORDER BY salary DESC
+LIMIT 10;
+
+SELECT 
+    *
+FROM
+    salaries
+ORDER BY emp_no DESC
+LIMIT 10;
+
+
+SELECT 
+    *
+FROM
+    dept_emp
+LIMIT 100;
