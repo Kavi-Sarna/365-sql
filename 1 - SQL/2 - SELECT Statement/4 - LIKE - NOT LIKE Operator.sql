b4496edc126e3ff8c92b680431c47d12c6e735ae
Kavi-Sarna <135118806+Kavi-Sarna@users.noreply.github.com>
2024-03-10 16:52:56 +0530

    LIKE - NOT LIKE Operator

---
+++ 1 - SQL/2 - SELECT Statement/4 - LIKE - NOT LIKE Operator.sql
@@ -0,0 +1,86 @@
+USE employees;
+
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name LIKE ('Mar%');
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name LIKE ('ar%');
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name LIKE ('%ar');
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name LIKE ('%ar%');
+
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name LIKE ('Mar_');
+
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name NOT LIKE ('%Mar%');
+
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name LIKE ('Mark%');
+
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    hire_date LIKE ('2000%');
+
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    emp_no LIKE ('1000_');
+
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name LIKE ('%Jack%');
+
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name NOT LIKE ('%Jack%');
